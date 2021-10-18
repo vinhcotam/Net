be28bc3f67e7de6366a75e58471feb9f2e2a2965
vinh4501 <vinhcotam04052001@gmail.com>
2021-10-18 20:56:39 +0700

    update nho nho

--- netstudio_demo/netstudio_demo/Login.Designer.cs
+++ netstudio_demo/netstudio_demo/Login.Designer.cs
@@ -31,10 +31,10 @@ namespace netstudio_demo
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbtkdn = new System.Windows.Forms.TextBox();
+            this.tbmkdn = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btdn = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
@@ -50,61 +50,66 @@ namespace netstudio_demo
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 47);
+            this.label1.Location = new System.Drawing.Point(22, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 15);
+            this.label1.Size = new System.Drawing.Size(97, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên tài khoản";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 98);
+            this.label2.Location = new System.Drawing.Point(22, 131);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.Size = new System.Drawing.Size(70, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Mật khẩu";
             // 
-            // textBox1
+            // tbtkdn
             // 
-            this.textBox1.Location = new System.Drawing.Point(103, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(219, 23);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.tbtkdn.Location = new System.Drawing.Point(118, 59);
+            this.tbtkdn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbtkdn.Name = "tbtkdn";
+            this.tbtkdn.Size = new System.Drawing.Size(250, 27);
+            this.tbtkdn.TabIndex = 2;
+            this.tbtkdn.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // tbmkdn
             // 
-            this.textBox2.Location = new System.Drawing.Point(103, 95);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(219, 23);
-            this.textBox2.TabIndex = 3;
+            this.tbmkdn.Location = new System.Drawing.Point(118, 127);
+            this.tbmkdn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbmkdn.Name = "tbmkdn";
+            this.tbmkdn.Size = new System.Drawing.Size(250, 27);
+            this.tbmkdn.TabIndex = 3;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btdn);
             this.groupBox1.Controls.Add(this.tableLayoutPanel1);
             this.groupBox1.Controls.Add(this.checkBox1);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.tbtkdn);
+            this.groupBox1.Controls.Add(this.tbmkdn);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(24, 12);
+            this.groupBox1.Location = new System.Drawing.Point(27, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(335, 222);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(383, 296);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Đăng nhập";
             // 
-            // button1
+            // btdn
             // 
-            this.button1.Location = new System.Drawing.Point(232, 136);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 43);
-            this.button1.TabIndex = 5;
-            this.button1.Text = " Đăng nhập";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btdn.Location = new System.Drawing.Point(265, 181);
+            this.btdn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btdn.Name = "btdn";
+            this.btdn.Size = new System.Drawing.Size(103, 57);
+            this.btdn.TabIndex = 5;
+            this.btdn.Text = " Đăng nhập";
+            this.btdn.UseVisualStyleBackColor = true;
+            this.btdn.Click += new System.EventHandler(this.button1_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -113,29 +118,31 @@ namespace netstudio_demo
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 190);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 253);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(316, 26);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(361, 35);
             this.tableLayoutPanel1.TabIndex = 6;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.label3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Location = new System.Drawing.Point(3, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(152, 20);
+            this.panel1.Size = new System.Drawing.Size(174, 27);
             this.panel1.TabIndex = 0;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.label3.Location = new System.Drawing.Point(44, 0);
+            this.label3.Location = new System.Drawing.Point(39, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(108, 15);
+            this.label3.Size = new System.Drawing.Size(135, 20);
             this.label3.TabIndex = 0;
             this.label3.Text = "Chưa có tài khoản?";
             // 
@@ -143,9 +150,10 @@ namespace netstudio_demo
             // 
             this.panel2.Controls.Add(this.linkLabel1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(161, 3);
+            this.panel2.Location = new System.Drawing.Point(183, 4);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(152, 20);
+            this.panel2.Size = new System.Drawing.Size(175, 27);
             this.panel2.TabIndex = 1;
             // 
             // linkLabel1
@@ -154,7 +162,7 @@ namespace netstudio_demo
             this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.linkLabel1.Location = new System.Drawing.Point(0, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(82, 15);
+            this.linkLabel1.Size = new System.Drawing.Size(103, 20);
             this.linkLabel1.TabIndex = 0;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Đăng ký ngay!";
@@ -162,19 +170,21 @@ namespace netstudio_demo
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(19, 149);
+            this.checkBox1.Location = new System.Drawing.Point(22, 199);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(102, 19);
+            this.checkBox1.Size = new System.Drawing.Size(124, 24);
             this.checkBox1.TabIndex = 4;
             this.checkBox1.Text = "hiện mật khẩu";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 238);
+            this.ClientSize = new System.Drawing.Size(438, 317);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
@@ -193,15 +203,16 @@ namespace netstudio_demo
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbtkdn;
+        private System.Windows.Forms.TextBox tbmkdn;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button log;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btdn;
     }
 }
